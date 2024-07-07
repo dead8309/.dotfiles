a01e2be31d3484af90a2c63648f80d58d2c3cd8d
dead8309 <vaibhavmonu8309@gmail.com>
2024-07-08 05:09:46 +0530

    fix harpoon replace keymaps
    add gruvbox

--- .config/nvim/lua/dead8309/lazy/harpoon.lua
+++ .config/nvim/lua/dead8309/lazy/harpoon.lua
@@ -14,9 +14,9 @@ return {
             vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
             vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
             vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-            vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-            vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+            vim.keymap.set("n", "<leader><M-1>", function() harpoon:list():replace_at(1) end)
+            vim.keymap.set("n", "<leader><M-2>", function() harpoon:list():replace_at(2) end)
+            vim.keymap.set("n", "<leader><M-3>", function() harpoon:list():replace_at(3) end)
+            vim.keymap.set("n", "<leader><M-4>", function() harpoon:list():replace_at(4) end)
     end
 }
