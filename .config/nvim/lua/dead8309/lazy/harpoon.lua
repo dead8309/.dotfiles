cc78df1cf597e1aa286ffd0657045b3f3888f414
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2025-02-13 10:53:52 +0530

    update harpoon
    highlight curr file

--- .config/nvim/lua/dead8309/lazy/harpoon.lua
+++ .config/nvim/lua/dead8309/lazy/harpoon.lua
@@ -4,8 +4,9 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function ()
         local harpoon = require("harpoon")
-
+        local harpoon_extensions = require("harpoon.extensions")
             harpoon:setup()
+            harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
 
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<M-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
