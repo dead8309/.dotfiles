343a97198866c01458883cbfc7ec0bcd67f0fce6
Vaibhav Raj <vaibhavmonu8309@gmai.com>
2025-06-20 15:28:51 +0530

    fix: spam clipboard notifs in wayland

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,7 +5,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard=unnamed,unnamedplus
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
